0a010f3cdfa4b8ffdf004a6799160f07ca766939
Snitrxm <andre.oliveira.roch@hotmail.com>
2022-08-17 21:51:44 -0300

    fix: test which not passed

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Soma(15,15)
+	total := soma(15,15)
 
 	if total != 30 {
 		t.Errorf("Soma incorreta, resultado: %d. Esperado: %d.", total, 30)
